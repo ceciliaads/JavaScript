f41775f3d83725385cd1eacec72e92731d60d715
Cecilia Da Silva <ceciliadasilva@Cecilias-MacBook-Air.local>
2020-08-11 20:24:29 -0300

    Guardo primeros tres ejercicios en un mismo repo

---
+++ 1Saludo/index.js
@@ -0,0 +1,23 @@
+let nombre = prompt("Cual es tu nombre ") + " " + prompt("y apellido?")
+
+alert(nombre )
+
+let saludo = "Hola "
+
+let saludoDeBienvenida = ", bienvenida a Ada"
+
+alert(saludo + nombre + saludoDeBienvenida)
+
+//RESUELTO POR PROFE
+
+// let nombreDeLaUsuaria = prompt("Decime tu nombre")
+
+// let apellidoDeLaUsuaria = prompt (nombreDeLaUsuaria + "Decime tu apellido")
+
+// alert ("Hola " + nombreDeLaUsuaria + " " + apellidoDeLaUsuaria + ", bienvenida a Ada")
+
+//o
+
+//let nombreCompletp = nombreDeLaUsuaria + " " + apellidoDeLaUsuaria
+
+//let saludo = "Hola " + nombreCompleto + ", bienvenida a Ada"
---
+++ 3Datos Personales/index.js
@@ -0,0 +1,11 @@
+let nombreCompleto = prompt("Decime tu Nombre") + " " + prompt("y Apellido")
+
+let edad = prompt("Decime tu Edad")
+
+let nacionalidad = prompt("Decime tu Nacionalidad")
+
+let documento = prompt("Decime tu DNI")
+
+let datosPersonales = nombreCompleto + " " + edad + " " + nacionalidad + " " + documento
+
+alert("Nuevo usuario agregado al sistema: " + datosPersonales)
